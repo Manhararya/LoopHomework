54418ca3a9de7dff386e6c304f7f350d76a59e64
manhar <manhararya25@gmail.com>
2023-04-07 13:07:44 +0100

    LoopHomework

--- StringCount.java
+++ StringCount.java
@@ -4,8 +4,10 @@ public class StringCount {
     public static void main(String[] args) {
         //Run this class and write any String and count total number of in that string
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter string here");
         String hello = scanner.nextLine();
         int charCount = hello.length() - hello.replaceAll("a", "").length();
+        System.out.println("Total number of 'a' is");
         System.out.println(charCount);
     }
     // Print total number of 'a' in string
